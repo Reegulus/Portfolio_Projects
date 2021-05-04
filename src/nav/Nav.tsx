e40ece0f60571c9981e0a6f6229bf94791f33f40
Egorych <mikhalevichegor@gmail.com>
2021-05-05 00:23:20 +0300

    Nav.css and Header.module.css - menu(flex)

--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import style from './Nav.module.css'
 
 export function Nav() {
-  return (
-    <div className={style.nav}>
-{/*<div>hi</div>*/}
-    </div>
-  );
+    return (
+        <div className={style.nav}>
+            <a href="">Home page</a>
+            <a href="">My skills</a>
+            <a href="">Portfolio</a>
+            <a href="">About</a>
+            <a href="">Contact</a>
+        </div>
+    );
 }
 
