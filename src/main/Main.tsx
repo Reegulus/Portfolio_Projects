d4f61e921c157eaf9b2142082c348feef492e715
Egorych <mikhalevichegor@gmail.com>
2021-05-05 12:36:41 +0300

    Main.tsx and Main.module.css - mainBlock(color and style)

---
+++ src/main/Main.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from './Main.module.css'
+
+export function Main() {
+    return (
+        <div className={style.mainBlock}>
+            <div className={style.container}>
+                <div className={style.greetings}>
+
+                </div>
+                <div className={style.avaPhoto}>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
