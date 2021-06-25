6fec74c03c41702b76b106b6250226ae57917cfd
Egorych <mikhalevichegor@gmail.com>
2021-06-25 22:22:18 +0300

    MyProjects.tsx component and MyProjects.module.css

---
+++ src/myProjects/MyProjects.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './MyProjects.module.css'
+
+export function MyProjects() {
+    return (
+        <div className={style.worksBlock}>
+
+        </div>
+    );
+}
+
---
+++ src/myProjects/work/MyProject.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './MyProject.module.css'
+
+export function MyProject() {
+    return (
+        <div className={style.workBlock}>
+
+        </div>
+    );
+}
+
